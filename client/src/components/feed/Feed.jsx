767c0a376a77ba5dfa7144fb052384ea2c67b279
Amy Posten <amyposten85@gmail.com>
2021-12-09 17:41:39 -0500

    fetches

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,17 +1,44 @@
-import "./feed.css"
-import Share from "../share/Share"
-import Post from "../post/Post"
-import {Posts} from "../../dummyData"
-
+import "./feed.css";
+import { useState, useEffect } from "react";
+import Share from "../share/Share";
+import Post from "../post/Post";
+// import {Posts} from "../../dummyData"
+///Fetch posts
 export default function Feed() {
-    return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share/>
-                {Posts.map((p) => (
-                    <Post key={p.id} post={p}/>
-                ))}
-            </div>
-        </div>
-    )
+  const [allPosts, setAllPosts] = useState([]);
+  useEffect(() => {
+    fetch("/posts")
+      .then((response) => response.json())
+      .then((allPosts) => {
+        setAllPosts(allPosts);
+      });
+  }, []);
+  // const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log("fuck");
+  //     setErrors([]);
+  //     setIsLoading(true);
+
+  //     fetch("/posts", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(newPost),
+  //     }).then(() => {
+  //       console.log("New Post Added");
+  //       console.log(newPost);
+  //       setIsLoading(false);
+  //       navigate("/");
+  //     });
+  //   };
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {allPosts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
