976eadcae963797c3951e8efea98a625625cdcee
Jui Hsu <artistray@gmail.com>
2021-12-09 11:12:36 -0500

    Share updated

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,33 +1,74 @@
-import "./share.css"
-import { PermMedia, Label, EmojiEmotions  } from "@material-ui/icons"
+import "./share.css";
+import { PermMedia, Label, EmojiEmotions  } from "@material-ui/icons";
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
-export default function Share() {
+const Share = () => {
+    const [content, setContent] = useState('')
+    const [imgurl, setImgurl] = useState('')
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();  
+    const newPost = { content, imgurl }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setErrors([])
+        setIsLoading(true);
+        setTimeout(() => {
+            fetch('/post', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newPost)
+            }).then(() => {
+                console.log("New Post Added")
+                console.log(newPost)
+                setIsLoading(false)
+                navigate('/');
+            })
+        },600)}
+    
+
     return (
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
                     <img className="shareProfileImg" src="/assets/person/pic1.jpg" />
-                    <input placeholder="What's in your mind?" className="shareInput" />
+                    <input 
+                        onSubmit={handleSubmit} 
+                        type="text" 
+                        placeholder="What's in your mind?" 
+                        className="shareInput" 
+                        onChange={(e) => setContent(e.target.value)}
+                    />
                 </div>
                 <hr className="shareHr"/>
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
                             <PermMedia htmlColor="tomato" className="shareIcon"/>
-                            <span className="shareOptionText">Photo or Video</span>
+                            <input 
+                                onSubmit={handleSubmit}
+                                type="text"
+                                placeholder="Type in image URL"
+                                className="shareOptionText"
+                                onChange={(e) => setImgurl(e.target.value)}
+                            />
                         </div>
-                        <div className="shareOption">
+                        {/* <div className="shareOption">
                             <Label htmlColor="lightgreen" className="shareIcon"/>
                             <span className="shareOptionText">Tag</span>
                         </div>
                         <div className="shareOption">
                             <EmojiEmotions htmlColor="goldenrod" className="shareIcon"/>
                             <span className="shareOptionText">Feeling</span>
-                        </div>
+                        </div> */}
                     </div>
-                    <button className="shareButton">Share</button>
+                    <button type="submit" className="shareButton">Share</button>
                 </div>
             </div>
         </div>
     )
 }
+
+export default Share;
